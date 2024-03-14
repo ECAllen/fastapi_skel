24b2ccb21f4a39f5e459941aa7cbe6ac96bab861
ECAllen <1305173+ECAllen@users.noreply.github.com>
2024-03-13 23:16:11 -0400

    added env, removed old code

--- dependencies.py
+++ dependencies.py
@@ -1,5 +1,12 @@
 from redislite import Redis
 from crud import SessionLocal
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+AlGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 def get_db():
     db = SessionLocal()
@@ -12,12 +19,3 @@ sessions = Redis("sessions.db")
 
 userids = Redis("userids.db")
 
-# TODO move this to .env
-
-##################################
-# to get a string like this run:
-# openssl rand -hex 32
-##################################
-SECRET_KEY = ""
-ALGORITHM = ""
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
--- schemas.py
+++ schemas.py
@@ -1,25 +1,6 @@
 from pydantic import BaseModel, Field
 
 
-class ProjectBase(BaseModel):
-    id: int
-    name: str
-    description: str | None = None
-    estimate: int | None = None
-
-
-class ProjectCreate(ProjectBase):
-    pass
-
-
-class Project(ProjectBase):
-    time: int | None = 0
-    start: int | None = 0
-
-    class Config:
-        from_attributes = True
-
-
 class UserBase(BaseModel):
     id: int
     email: str | None = None
@@ -34,15 +15,6 @@ class User(UserBase):
     is_active: bool
     projects: list[Project] = []
     full_name: str | None = None
-
-    class Config:
-        from_attributes = True
-
-
-class UserProject(BaseModel):
-    id: int
-    user_id: int
-    project_id: int
 
     class Config:
         from_attributes = True
